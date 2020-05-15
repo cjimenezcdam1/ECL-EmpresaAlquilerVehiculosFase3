59f0ce65073e5e400af8fab4f26717846e5a5ab9
cjimenezcdam1 <cjimenezcdam1@gmail.com>
2020-05-15 12:51:57 +0200

    ETIQUETA AUTHOR Y LIBRERÍAS - Se importan las liberrías JavaFX al proyecto - Se añade la etiqueta author

--- src/alquileres/interfaz/GuiAgenciaAlquiler.java
+++ src/alquileres/interfaz/GuiAgenciaAlquiler.java
@@ -13,7 +13,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+/**
+ * 
+ * @author Christian
+ *
+ */
 public class GuiAgenciaAlquiler extends Application
 {
 
